0f57a4349c31212afa58db4c826aa6f38e23166b
Benjamin Gudehus <hastebrot@gmail.com>
2016-09-24 16:47:19 +0200

    (refactor) Add interface for HelloMessage props.

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,9 +1,13 @@
 declare let mountNode: any
 
-class HelloMessage extends React.Component<any, any> {
+class HelloMessage extends React.Component<HelloMessageProps, any> {
   render() {
     return <div>Hello {this.props.name}</div>
   }
+}
+
+interface HelloMessageProps {
+  name: string
 }
 
 ReactDOM.render(<HelloMessage name="John" />, mountNode)
