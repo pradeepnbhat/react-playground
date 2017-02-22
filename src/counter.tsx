cc8414aa9f2cfdd8a074d6c10a654b5c21148706
Pradeep Narayan <pradeepbhat@live.com>
2017-02-22 15:33:15 -0800

    Sample changes

--- src/counter.tsx
+++ src/counter.tsx
@@ -1,15 +1,106 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { createStore } from "redux"
-
-class HelloMessage extends React.Component<HelloMessageProps, any> {
-  render() {
-    return <div>Hello {this.props.name}</div>
-  }
+interface CounterProps {
+   init: number;
 }
 
-interface HelloMessageProps {
-  name: string
+interface CounterState {
+  current: number;
+  command: string;
 }
 
-ReactDOM.render(<HelloMessage name="John" />, mountNode)
+class Counter extends React.Component<CounterProps, CounterState>
+ {
+    constructor(props: CounterProps){
+      super(props)
+      this.state = { current: this.props.init, command: "Start" }
+    }
+
+    // Called before the component is mounted
+    public componentWillMount(){
+      console.log("componentWillMount called");
+    }
+
+    public componentDidMount(){
+      console.log("componentDidMount called");
+    }
+
+    public componentWillUnmount(){
+      console.log("componentWillUnmount called");
+      if (this._timerId){
+        clearInterval(this._timerId);
+      }
+    }
+
+    public shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState){
+      console.log("shouldComponentUpdate called");
+      return (this.state.current !== nextState.current || this.state.command !== nextState.command);
+    }
+
+    public componentWillUpdate(){
+      console.log("componentWillUpdate called");
+    }
+
+    public componentDidUpdate(){
+      console.log("componentDidUpdate called");
+    }
+
+    public render() {
+      console.log("render called")
+      return (
+        <div>
+          <div>Counter: {this.state.current}</div>
+          <button onClick={this._handleClick}>{this.state.command}</button>
+        </div>
+      )
+    }
+
+    // Notice the delegate.
+    private _handleClick = () => {
+
+      // Partial updates. 
+      this.setState((prevState: CounterState, props: CounterProps) => {
+
+
+        if (prevState.command === "Start"){
+          this._timerId = window.setInterval(this._handleTick, 1000);  
+          return {
+              command: "Stop"
+          } as CounterState;
+        }
+        else if (prevState.command === "Stop"){
+          window.clearInterval(this._timerId);
+          return {
+              command: "Start"
+          } as CounterState;
+        }
+      })
+    }
+
+    // Notice the delegate.
+    private _handleTick = () => {
+
+      // What is the error in this code?
+      // How to correct it?
+      this.setState({
+         current: this.state.current + 1
+      } as CounterState);
+    }
+
+    private _timerId: number;
+}
+
+
+ReactDOM.render(<Counter init={10} />, mountNode);
+
+/*
+
+this.setState({
+          current: this.state.current + 1
+        } as CounterState);
+
+this.setState((prevState: CounterState, props: CounterProps) => {
+        return {
+          current: prevState.current + 1,
+          personToWish: prevState.current > 10 ? "Pradeep" : "React" 
+        }
+      });
+*/
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,13 +1,38 @@
 declare let mountNode: any
 
+interface HelloMessageProps {
+  name: string;
+  date: Date;
+}
+
 class HelloMessage extends React.Component<HelloMessageProps, any> {
+
   render() {
-    return <div>Hello {this.props.name}</div>
+    return <div>Hello {this.props.name} ! How are you on {this.props.date.toDateString()} </div>
   }
 }
 
-interface HelloMessageProps {
-  name: string
+class HelpMessage extends React.Component<{}, {}> {
+  render(){
+    return (
+      <div>
+        How can I help you?
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<HelloMessage name="John" />, mountNode)
+class Greetings extends React.Component<{}, {}> {
+
+  render() {
+    return (
+      <div>
+        <HelloMessage name="Pradeep" date={new Date()} />
+        <HelpMessage />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Greetings />, mountNode)
